1418ff706011e7a3d517a3d2397c82d278a7d3e8
Maixent Chenebaux <max.chbx@gmail.com>
2020-11-19 17:02:10 +0100

    added git url

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
     author="Maixent Chenebaux",
     author_email="max.chbx@gmail.com",
     description="Fast random walks on graph",
+    url="https://github.com/kerighan/graph-walker",
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=["networkx", "numpy", "scipy", "numba"],
