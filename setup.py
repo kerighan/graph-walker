76fb1971eb59610a2495750221170ae5b3ab457c
Maixent Chenebaux <max.chbx@gmail.com>
2021-02-03 11:29:17 +0100

    fixed error in setup

--- setup.py
+++ setup.py
@@ -13,12 +13,22 @@ ext_modules = [
         include_dirs=[
             pybind11.get_include(),
             pybind11.get_include(True),],
-        extra_compile_args=compiler_args)]
+        extra_compile_args=compiler_args)
+]
 
 
 setup(
-    name="walker",
+    name="graph-walker",
     version="1.0.0",
+    author="Maixent Chenebaux",
+    author_email="max.chbx@gmail.com",
+    description="Fastest library for random walks on graph",
+    url="https://github.com/kerighan/graph-walker",
     packages=find_packages(),
+    install_requires=["networkx", "numpy", "scipy", "pybind11"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     ext_modules=ext_modules
 )
